2bf552cca82f1b44c58ed2a0ff30a4c5499f870e
Kilian Koeltzsch <me@kilian.io>
2018-01-16 11:40:15 +0100

    add homebrew support

---
+++ Formula/wikitranslate.rb
@@ -0,0 +1,16 @@
+class WikiTranslate < Formula
+    desc "Use Wikipedia as a dictionary"
+    homepage "https://github.com/kiliankoe/WikiTranslate"
+    url "https://github.com/kiliankoe/WikiTranslate/archive/0.1.0.tar.gz"
+    sha256 "f2fcef3e577b2ed679d9f69490d585af2b97be0567c7baef481990e2ee15c36b"
+    head "https://github.com/kiliankoe/WikiTranslate.git"
+
+    depends_on :xcode
+
+    def install
+        build_path = "#{buildpath}/.build/release/wt"
+        ohai "Building WikiTranslate"
+        system("swift build -c release --static-swift-stdlib")
+        bin.install build_path
+    end
+end
