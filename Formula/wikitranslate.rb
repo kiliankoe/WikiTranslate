6dac917cf782ec892abcd92af5fe566de3775ebd
Kilian Koeltzsch <me@kilian.io>
2018-07-28 13:01:56 +0200

    bump homebrew formula

--- Formula/wikitranslate.rb
+++ Formula/wikitranslate.rb
@@ -1,8 +1,8 @@
 class Wikitranslate < Formula
     desc "Use Wikipedia as a dictionary"
     homepage "https://github.com/kiliankoe/WikiTranslate"
-    url "https://github.com/kiliankoe/WikiTranslate/archive/0.1.0.tar.gz"
-    sha256 "f2fcef3e577b2ed679d9f69490d585af2b97be0567c7baef481990e2ee15c36b"
+    url "https://github.com/kiliankoe/WikiTranslate/archive/0.1.1.tar.gz"
+    sha256 "977af8fef5348b1750d579cf4d7c9c95aff34933b714532fc87193f9d7404328"
     head "https://github.com/kiliankoe/WikiTranslate.git"
 
     depends_on :xcode
