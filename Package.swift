6766c76a05b414076540da3831e269749156e22e
Kilian Koeltzsch <me@kilian.io>
2018-07-28 12:58:39 +0200

    update dependencies

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
         .executable(name: "wt", targets: ["wt"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kiliankoe/CLISpinner", .upToNextMajor(from: "0.3.5")),
-        .package(url: "https://github.com/IBM-Swift/BlueSignals", .upToNextMajor(from: "0.9.50")),
+        .package(url: "https://github.com/kiliankoe/CLISpinner", .upToNextMajor(from: "0.3.6")),
+        .package(url: "https://github.com/IBM-Swift/BlueSignals", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/Raureif/WikipediaKit", .revision("v1.0.0-beta.5")),
     ],
     targets: [
